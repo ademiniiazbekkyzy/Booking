98ff91482ec170d8020f3c074c386aca62eba13b
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2022-12-18 13:38:43 +0600

    deploying issuessss

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'booking_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRES_HOST'),
-        'PORT': config('POSTGRES_PORT', cast=int),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('POSTGRES_DB'),
+#         'USER': config('POSTGRES_USER'),
+#         'PASSWORD': config('POSTGRES_PASSWORD'),
+#         'HOST': config('POSTGRES_HOST'),
+#         'PORT': config('POSTGRES_PORT', cast=int),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -101,6 +101,14 @@ DATABASES = {
 #         'PORT': 5432,
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-b647.up.railway.app']
