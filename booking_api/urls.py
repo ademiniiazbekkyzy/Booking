3cfbc9911e036ca077a96c683d344e8871adb8c8
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2023-04-15 22:32:57 +0600

    added swagger

--- booking_api/urls.py
+++ booking_api/urls.py
@@ -20,13 +20,21 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Booking project',
+        description='Booking of facilities',
+        default_version='v1',
+    ),
+    public=True
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('element/', include('main.urls')),
-    # path('swagger/', schema_view.with_ui('swagger')),
+    path('swagger/', schema_view.with_ui('swagger')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
